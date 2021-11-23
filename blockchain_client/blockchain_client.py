c0a4b686e1973dd2241dde96ad72a5d547633f1a
DavidG <dg_codemonkey@outlook.com>
2021-11-23 19:41:16 +0800

    Added pages, scripts for creating a wallet

---
+++ blockchain_client/blockchain_client.py
@@ -0,0 +1,46 @@
+
+from flask import Flask, render_template
+
+
+class Transaction:
+    def __init__(self, sender_address, sender_private_key, receiver_address, value):
+        self.sender_address = sender_address
+        self.sender_private_key = sender_private_key
+        self.receiver_address = receiver_address
+        self.value = value
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')  # Flask will look inside templates/
+
+
+@app.route("/create/transaction")
+def create_transaction():
+    # Flask will look inside templates/
+    return render_template('create_transaction.html')
+
+
+@app.route("/view/transactions")
+def view_transactions():
+    # Flask will look inside templates/
+    return render_template('view_transactions.html')
+
+
+@app.route("/create/wallet")
+def create_wallet():
+    # Flask will look inside templates/
+    return render_template('create_wallet.html')
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=8081,
+                        type=int, help="Port to listen to")
+    args = parser.parse_args()
+    port = args.port
+    app.run(host='127.0.01', port=port, debug=True)
---
+++ tests/test_Transaction.py
@@ -0,0 +1,4 @@
+from blockchain_client import blockchain_client
+
+def test_create_Transation():
+  pass
